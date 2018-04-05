cf8c959c798173327652e4874e7c67e093768aaf
Unknown <cerny.vil@gmail.com>
2018-04-05 15:04:06 +0200

    + transitions

--- src/models/Skill.php
+++ src/models/Skill.php
@@ -12,29 +12,30 @@ class Skill extends Model
 
 	protected $appends = ['category','details'];
 
-	public function categorySkill(){
-		return $this->belongsTo('Vilbur\Skills\Models\CategorySkill');
+	public function categorySkill()
+	{
+		return $this->belongsTo('Vilbur\Skills\Models\CategorySkill', 'category_id');
 	}
-	public function skillDetails(){
+	
+	public function skillDetails()
+	{
 		return $this->hasMany('Vilbur\Skills\Models\SkillDetail');
 	}
-
 
     //public function getSinceAttribute($date)
     //{
     //    return Carbon::createFromFormat('Y-m-d', $date)->diffForHumans();
     //}
+	
     public function getDetailsAttribute($value)
     {
         return $this->skillDetails->toArray();
     }
 
-	/**
-	*/
 	public function getCategoryAttribute($value)
 	{
 		//dump( $this->categorySkill->title );
-		return $this->categorySkill()->first()->title;
+		return $this->categorySkill->slug;
 		//return $this->categorySkill->first()->title();
 	}
 
