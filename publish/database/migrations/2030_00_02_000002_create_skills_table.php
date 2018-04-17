de8d3e6508971922fa42f0179caa8fafc1df52a4
Unknown <cerny.vil@gmail.com>
2018-04-17 09:16:40 +0200

    category_skill_id changed to category_id

--- publish/database/migrations/2030_00_02_000002_create_skills_table.php
+++ publish/database/migrations/2030_00_02_000002_create_skills_table.php
@@ -15,12 +15,13 @@ class CreateSkillsTable extends Migration
 		Schema::create('skills', function(Blueprint $table) {
 			$table->increments('id');
 			
-			$table->integer('category_skill_id')->nullable()->unsigned()->index();
-			$table->foreign('category_skill_id')->references('id')->on('category_skills')->onDelete('cascade');
+			$table->integer('category_id')->nullable()->unsigned()->index();
+			$table->foreign('category_id')->references('id')->on('category_skills')->onDelete('cascade');
 
 			$table->string('title',\Config::get('migrations.lenght.title'))->nullable();
 			$table->string('summary',\Config::get('migrations.lenght.summary'))->nullable();
 			$table->string('description',\Config::get('migrations.lenght.description'))->nullable();
+			$table->string('image',	\Config::get('migrations.image'))->nullable();
 
 			$table->timestamps();
 		});
