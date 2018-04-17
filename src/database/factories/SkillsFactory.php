39be22a8ea6beb6a3390d29858b7726a9317f086
Unknown <cerny.vil@gmail.com>
2018-04-17 09:23:05 +0200

    category_skill_id changed to category_id

--- src/database/factories/SkillsFactory.php
+++ src/database/factories/SkillsFactory.php
@@ -13,7 +13,7 @@ $factory->define(\Vilbur\Skills\Models\Skill::class, function (Faker $faker) {
     return [
 		'title'	=> $faker->text(24),
 		'summary'	=> $faker->sentence(6, true),
-		'category_skill_id'	=> $faker->randomElement(\DB::table('category_skills')->select('id')->get()->toArray())->id,
+		'category_id'	=> $faker->randomElement(\DB::table('category_skills')->select('id')->get()->toArray())->id,
 		//////'description'	=> $faker->text(128),
     ];
 });
